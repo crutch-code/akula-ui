c7205c32e4f5cf3fd01c81eb865ee75b38de7a44
mr.tsowa <tsowa48@gmail.com>
2024-04-19 13:09:53 +0300

    v0.0.27: test impl 2

--- src/components/parts/AddQuestionSingle.tsx
+++ src/components/parts/AddQuestionSingle.tsx
@@ -1,10 +1,10 @@
-import React, {ReactElement, useEffect, useState} from "react";
+import React, {ChangeEvent, ReactElement, useEffect, useState} from "react";
 import {AnswerType, QuestionType} from "../../types/QuestionType";
 import {Button} from "../Button";
 
 export function AddQuestionSingle(props: any): ReactElement {
     const qIndex = props.index!;
-    const question: QuestionType = props.question;
+    let question: QuestionType = props.question;
 
     const [answers, setAnswers] = useState<AnswerType[]>([]);
 
@@ -34,7 +34,10 @@ export function AddQuestionSingle(props: any): ReactElement {
                     padding: "6px 10px 0 0",
                     whiteSpace: "nowrap"
                 }}>Текст вопроса:</label>
-                <input type="text" placeholder={"Текст вопроса"} style={{width: "100%"}} defaultValue={question.title}/>
+                <input type="text" placeholder={"Текст вопроса"} style={{width: "100%"}} defaultValue={question.title}
+                       onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                           question.title = e.target.value;
+                       }}/>
             </div>
             <div className={"inputGroup"} style={{padding: "0 0 15px 0", display: "flex", width: "100%"}}>
                 <label style={{
@@ -43,7 +46,10 @@ export function AddQuestionSingle(props: any): ReactElement {
                     padding: "6px 10px 0 0",
                     whiteSpace: "nowrap"
                 }}>Балл:</label>
-                <input type="number" min={1} placeholder={"Балл за вопрос"} style={{width: "100%"}} defaultValue={question.points}/>
+                <input type="number" min={1} placeholder={"Балл за вопрос"} style={{width: "100%"}}
+                       defaultValue={question.points} onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                    question.points = parseInt(e.target.value);
+                }}/>
             </div>
 
             {answers.map((a, i) =>
@@ -56,8 +62,15 @@ export function AddQuestionSingle(props: any): ReactElement {
                         whiteSpace: "nowrap"
                     }}>{"Ответ " + a._index + ":"}</label>
                     <input type="radio" style={{marginRight: "8px"}} defaultChecked={a.correct ?? false}
-                           name={"q" + qIndex}></input>
-                    <input type="text" placeholder={"Вариант ответа"} style={{width: "100%"}} defaultValue={a.content}/>
+                           name={"q" + qIndex} onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                        a.correct = e.target.checked
+                        question.answers = answers;
+                    }}/>
+                    <input type="text" placeholder={"Вариант ответа"} style={{width: "100%"}} defaultValue={a.content}
+                           onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                               a.content = e.target.value
+                               question.answers = answers;
+                           }}/>
                 </div>
             )}
             <Button text={"Добавить вариант ответа"} onClick={() => addAnswer()}/>
